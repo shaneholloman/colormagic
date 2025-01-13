15878a7be07c4a963def46e1e4346821cc8b593d
Tim <tim103@pm.me>
2025-01-13 15:35:25 +1100

    Improvement to naming.

--- layers/palette/server/helpers/palette.helper.ts
+++ layers/palette/server/helpers/palette.helper.ts
@@ -44,6 +44,7 @@ export function mapCreatePalettePrompt(prompt: string): string {
   value += 'Align the tones. Avoid really close hues (can be a bit close). Eg. "sunset" would be #1e507b #4f6187 #8587a3 #f1b341 #d63f2e';
   value += 'If the input is hex colors then generate a similar palette. Doesn\'t have to be exactly the same, make it nicer.';
   value += 'Also create a name for this palette (can use input for it if not rude) and format like this: [name:Beach] or [name:Red Car]';
+  value += 'If the input contains a color, you can use it in the name (but don\'t have to)';
   value += 'Also create up to 5 tags we can use for filtering, including a tag for each color and format like this: [tags:beach,sand,blue,warm,dark] etc.';
   /** @description use the filter color tags so it knows some good examples */
   value += `Only pick tags from the following: ${getAllPaletteFilters().map(color => color.id).join(',')},${getPaletteCombos().map(v => v.join('-')).join(',')}`;
